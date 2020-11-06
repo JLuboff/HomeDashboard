0c99da2caaf65097253f21e099c8842bffdd85be
Jason Luboff <jluboff@gmail.com>
2020-11-06 15:46:09 -0800

    Successfully open/close garage door. Need to modulize and/or turn into a class

--- server/controllers/MyQGarage.ts
+++ server/controllers/MyQGarage.ts
@@ -1,28 +1,43 @@
 import MyQ from 'myq-api';
 import path from 'path';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
 
 const MyQGarage = async () => {
-const myQ = new MyQ();
-const getToken = async () => {
+  const myQ = new MyQ();
+  const getToken = async (): Promise<void> => {
     try {
-        const result = await myQ.login(process.env.MYQEMAIL, process.env.MYQPASSWORD);
-
-        return result
+      await myQ.login(process.env.MYQEMAIL, process.env.MYQPASSWORD);
     } catch (error) {
-        console.error(error)
+      console.error(error);
     }
-} 
+  };
 
-async function connected(){
+  async function connected() {
     try {
-        const token = await getToken();
-        const deviceList = await token.getDevices();
-    } catch (error) {
-        console.error(error)
-    }
-}
+      await getToken();
 
-connected();
-}
+      const deviceList = await myQ?.getDevices();
+      const garageDoor = deviceList?.devices?.filter(
+        ({ device_family }) => device_family === 'garagedoor'
+      )[0];
+      const garageDoorSerialNumber = garageDoor.serial_number;
+      const garageDoorStatus = await  myQ?.getDoorState(garageDoorSerialNumber);
+
+      if (garageDoorStatus.deviceState === 'closed') {
+        console.log(MyQ.actions)
+        await myQ?.setDoorState(garageDoorSerialNumber, MyQ.actions.door.OPEN)
+      }
+      if (garageDoorStatus.deviceState === 'open'){
+        await myQ?.setDoorState(garageDoorSerialNumber, MyQ.actions.door.CLOSE)
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  connected();
+};
 
 export default MyQGarage;
