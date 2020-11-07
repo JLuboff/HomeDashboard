a0df6d4102da20a8307fb1b65036e07659f246a8
Jason Luboff <jluboff@gmail.com>
2020-11-07 13:26:38 -0800

    Converted MyQGarage to a class. Need to still take care of error handling as well as write all of the interfaces (same are written but incomlete). Server entry file currently is just for testing.

--- server/controllers/MyQGarage.ts
+++ server/controllers/MyQGarage.ts
@@ -6,24 +6,77 @@ dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
 
 enum GARAGEDOORSTATUS {
   OPEN,
-  CLOSED
+  CLOSED,
 }
 
-class MyQGarageClass {
+type DeviceFamily = 'garagedoor' | 'light' | 'gateway';
+type DeviceType = 'wifigdogateway' | 'wifigaragedooropener';
+
+export interface MyQDevice {
+  created_date: string;
+  device_family: DeviceFamily;
+  device_platform: string;
+  device_type: DeviceType;
+  href: string;
+  name: string;
+  parent_device: string;
+  parent_device_id: string;
+  serial_number: string;
+}
+
+interface MyQAPI {
+  _accountId: null | string;
+  _devices: MyQDevice[];
+  _securityToken: null | string;
+  _executeServiceRequest: () => Promise<any>;
+  _getAccountId: () => Promise<any>;
+  _getDeviceState: () => Promise<any>;
+  _setDeviceState: () => Promise<any>;
+  getDevice: () => Promise<any>;
+  getDevices: () => Promise<any>;
+  getDoorState: (serialNumber: string) => Promise<any>;
+  getLightState: () => Promise<any>;
+  login: (email: string, password: string) => Promise<any>;
+  setDoorState: (
+    serialNumber: string,
+    status: 'OPEN' | 'CLOSE'
+  ) => Promise<any>;
+  setLightState: () => Promise<any>;
+}
+
+interface MyQGarageClass {
   email: string;
   password: string;
-  myQ: any = new MyQ()
-
-  constructor(email: string, password: string){
-    this.email = email,
-    this.password = password
-  }
+  myQ: any;
+  login: () => Promise<void>;
+  getGarageDoors: (
+    singleGarage: boolean
+  ) => Promise<MyQDevice | MyQDevice[] | undefined>;
+  getGarageDoorStatus: (
+    serialNumber: string
+  ) => Promise<keyof typeof GARAGEDOORSTATUS | undefined>;
+  changeGarageDoorStatus: (
+    serialNumber: string,
+    status: keyof typeof GARAGEDOORSTATUS
+  ) => Promise<void>;
+}
 /**
- * Logs in user and gets securityToken along with associated devices.
- * Devices get attached to myQ object.
- * @async
+ * Instanitiates a new MyQGarage class, exposing all class methods and members
  */
-  async login(){
+export default class MyQGarage implements MyQGarageClass {
+  email: string;
+  password: string;
+  myQ: MyQAPI = new MyQ();
+
+  constructor(email: string, password: string) {
+    (this.email = email), (this.password = password);
+  }
+  /**
+   * Logs in user and gets securityToken along with associated devices.
+   * Devices get attached to myQ object.
+   * @async
+   */
+  async login() {
     try {
       await this.myQ.login(this.email, this.password);
       await this.myQ.getDevices();
@@ -34,20 +87,20 @@ class MyQGarageClass {
       console.error(error);
     }
   }
-/**
- * Returns single garage object if singleGarage is true else returns an array of garage objects
- * @async
- * @param singleGarage 
- * boolean
- */
-  async getGarageDoors(singleGarage: boolean){
+  /**
+   * Returns single garage object if singleGarage is true else returns an array of garage objects
+   * @async
+   * @param singleGarage
+   * boolean
+   */
+  async getGarageDoors(singleGarage: boolean) {
     try {
-      const garageDoor = this.myQ.deviceList?.devices?.filter(
+      const garageDoor = this.myQ._devices?.filter(
         ({ device_family }) => device_family === 'garagedoor'
-      )
+      );
 
-      if (singleGarage){
-        return garageDoor[0]
+      if (singleGarage) {
+        return garageDoor[0];
       }
 
       return garageDoor;
@@ -58,13 +111,15 @@ class MyQGarageClass {
       console.error(error);
     }
   }
-/**
- * Returns the current garage door status (open or closed)
- * @async
- * @param serialNumber 
- * string
- */
-  async getGarageDoorStatus(serialNumber: string): Promise<keyof typeof GARAGEDOORSTATUS | undefined> {
+  /**
+   * Returns the current garage door status (open or closed)
+   * @async
+   * @param serialNumber
+   * string
+   */
+  async getGarageDoorStatus(
+    serialNumber: string
+  ): Promise<keyof typeof GARAGEDOORSTATUS | undefined> {
     try {
       return await this.myQ?.getDoorState(serialNumber);
     } catch (error) {
@@ -74,10 +129,16 @@ class MyQGarageClass {
       console.error(error);
     }
   }
-  
-  async changeGarageDoorStatus(serialNumber: string, status: keyof typeof GARAGEDOORSTATUS){
+
+  async changeGarageDoorStatus(
+    serialNumber: string,
+    status: keyof typeof GARAGEDOORSTATUS
+  ) {
     try {
-        await this.myQ?.setDoorState(serialNumber, status === 'OPEN' ? MyQ.actions.door.OPEN : MyQ.actions.door.CLOSE);
+      await this.myQ?.setDoorState(
+        serialNumber,
+        status === 'OPEN' ? MyQ.actions.door.OPEN : MyQ.actions.door.CLOSE
+      );
     } catch (error) {
       /**
        * TODO: Handle error properly
@@ -86,41 +147,3 @@ class MyQGarageClass {
     }
   }
 }
-
-const MyQGarage = async () => {
-  const myQ = new MyQ();
-  const getToken = async (): Promise<void> => {
-    try {
-      await myQ.login(process.env.MYQEMAIL, process.env.MYQPASSWORD);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  async function connected() {
-    try {
-      await getToken();
-
-      const deviceList = await myQ?.getDevices();
-      const garageDoor = deviceList?.devices?.filter(
-        ({ device_family }) => device_family === 'garagedoor'
-      )[0];
-      const garageDoorSerialNumber = garageDoor.serial_number;
-      const garageDoorStatus = await  myQ?.getDoorState(garageDoorSerialNumber);
-
-      if (garageDoorStatus.deviceState === 'closed') {
-        console.log(MyQ.actions)
-        await myQ?.setDoorState(garageDoorSerialNumber, MyQ.actions.door.OPEN)
-      }
-      if (garageDoorStatus.deviceState === 'open'){
-        await myQ?.setDoorState(garageDoorSerialNumber, MyQ.actions.door.CLOSE)
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  connected();
-};
-
-export default MyQGarage;
--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,13 @@
-import MyQGarage from './controllers/MyQGarage';
+import MyQGarage, { MyQDevice} from './controllers/MyQGarage';
 
+async function Server(){
+const MyQ = new MyQGarage(
+  process.env.MYQEMAIL ?? '',
+  process.env.MYQPASSWORD ?? ''
+);
+await MyQ.login();
+const garageDoor = await MyQ.getGarageDoors(true);
+const garageDoorStatus = await MyQ.getGarageDoorStatus((garageDoor as MyQDevice)?.serial_number)
+}
 
-MyQGarage();
+Server();
